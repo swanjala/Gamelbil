99b3aa4236b49b7668ad798326ab1ab756464c0c
swanjala <wanjala.sam.s@gmail.com>
2019-02-23 18:19:49 +0300

    --amend

--- src/main/java/GamelbilApp.java
+++ src/main/java/GamelbilApp.java
@@ -1,6 +1,9 @@
+import datapreprocessor.ImportData;
+
 public class GamelbilApp {
 
     public static void main(String [] args){
-
+        ImportData importData  = new ImportData();
+        importData.dataCollection();
     }
 }
