ff74dae43a9d654409df2c36c3170fa297ba54f1
swanjala <wanjala.sam.s@gmail.com>
2019-03-21 00:58:57 +0300

    (add): tree builder class
    - Implement tree builder thread
    - Run tree builder
    - Print tree nodes

--- src/main/java/algoritms/trees/treeTraversal/TreeBuilder.java
+++ src/main/java/algoritms/trees/treeTraversal/TreeBuilder.java
@@ -1,4 +1,33 @@
 package algoritms.trees.treeTraversal;
 
-public class TreeBuilder {
+import algoritms.trees.print_service.AbstractTreePrinter;
+
+public class TreeBuilder implements Runnable {
+
+    public final AbstractTreePrinter iTreePrinter = new BinaryTree();
+
+    public void run() {
+
+        try {
+
+            ((BinaryTree) iTreePrinter).root = new BinaryTreeNodes('1');
+            ((BinaryTree) iTreePrinter).root.left = new BinaryTreeNodes('2');
+            ((BinaryTree) iTreePrinter).root.right = new BinaryTreeNodes('3');
+            ((BinaryTree) iTreePrinter).root.left.left = new BinaryTreeNodes('4');
+            ((BinaryTree) iTreePrinter).root.left.right = new BinaryTreeNodes('5');
+
+        } finally {
+
+            System.out.println("Preorder traversal of binary tree is");
+            iTreePrinter.printPreOrder();
+
+            System.out.println("In Order traversal of binary tree is");
+            iTreePrinter.printInOrder();
+
+            System.out.println("Post Order traversal of binary tree is");
+            iTreePrinter.printPostOrder();
+
+        }
+    }
+
 }
