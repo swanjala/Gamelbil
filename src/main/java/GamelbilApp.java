1e6ac9a600bb073b0b932cd1124d0d6308b1f086
swanjala <wanjala.sam.s@gmail.com>
2019-04-12 23:22:04 +0300

    (update): remove unused code

--- src/main/java/GamelbilApp.java
+++ src/main/java/GamelbilApp.java
@@ -8,16 +8,5 @@ import java.util.Scanner;
 public class GamelbilApp {
 
     public static void main(String [] args){
-
-//        Scanner scanner = new Scanner(System.in);
-//        ImportData importData  = new ImportData();
-//        //Tag tag = new Tag(importData.dataCollection());
-//        tag.TagData();
-//        BinarySearch binarySearch = new BinarySearch(tag.TagData(),
-//               tag.keyData());
-//        System.out.println("Enter a car id");
-//        String value = scanner.next();
-//
-//        binarySearch.search(Integer.parseInt(value));
     }
 }
